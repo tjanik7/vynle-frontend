2e7b83f42d07ddcd1cddadf25061b01ca19d4208
Ty Janik <tyjanik44@gmail.com>
2025-02-19 09:26:28 -0500

    Fixed bug in form validation (function now makes sure the field is in the form before asserting it must be populated)

--- src/components/accounts/formValidator.js
+++ src/components/accounts/formValidator.js
@@ -1,28 +1,33 @@
 export default function formHasError(fields, setErrors) {
+    function fieldIncomplete(fieldName) {
+        // Make sure the field exists in the form and check if filled out
+        return fieldName in fields && !fields[fieldName]
+    }
+
     // Catch obvious form errors here before submitting
 
     const errorMessages = {}
     let errorFound = false
 
-    if (!fields.email) {
+    if (fieldIncomplete('email')) {
         errorMessages['email'] = ['This field is required']
         errorFound = true
     }
 
-    if (!fields.username) {
+    if (fieldIncomplete('username')) {
         errorMessages['username'] = ['This field is required']
         errorFound = true
     }
 
-    if (!fields.password) {
+    if (fieldIncomplete('password')) {
         errorMessages['password'] = ['This field is required']
         errorFound = true
     }
 
-    if (!fields.re_password) {
+    if (fieldIncomplete('re_password')) {
         errorMessages['re_password'] = ['This field is required']
         errorFound = true
-    } else if (fields.password !== fields.re_password) {
+    } else if ('password' in fields && 're_password' in fields && fields.password !== fields.re_password) {
         errorMessages['re_password'] = ['Passwords do not match']
         errorFound = true
     }
