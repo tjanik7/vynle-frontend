2b2305349042096c1e50fe0cc7f8bcd95ce4ebe7
Ty Janik <tyjanik44@gmail.com>
2025-09-26 15:29:38 -0400

    Issue: vynle-15
    - Refactored CoverArt component to be functional (was class based)
    - Removed references to an "index" in CoverArt. We now React "refs" within the component to reference the specific element to be updated
    - Form.js (new post creation form) sets the "fetched" field to true (within the albumData object) since this is how CoverArt
    determines whether the release data is ready
    - Cleaned up CoverArt.css

--- src/components/profile/FavoriteAlbums.js
+++ src/components/profile/FavoriteAlbums.js
@@ -1,77 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import { Col, Container, Row } from "react-bootstrap"
-import CoverArt from "../cover_art/CoverArt"
-import PropTypes from "prop-types"
-import Search from "../search/Search"
-import { setSelectedIndex } from "../../actions/spotifySearch"
-import { connect } from "react-redux"
-import { setFavAlbum } from "../../actions/profile"
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import CoverArt from "../cover_art/CoverArt";
+import PropTypes from "prop-types";
+import Search from "../search/Search";
+import { setSelectedIndex } from "../../actions/spotifySearch";
+import { connect } from "react-redux";
+import { setFavAlbum } from "../../actions/profile";
 
 function generateAlbumTags(props, setSearchDisplayed, isClickable) {
-    const columns = []
+  const columns = [];
 
-    for (let i = 0; i < 6; i++) {  // Generates JSX tags for album art
-        columns.push(
-            <Col key={i}>
-                <CoverArt
-                    albumData={props.profile.favoriteAlbums[i]}
-                    isClickable={isClickable}
-                    fontSize={11}
-                    ind={i}
-                    displayReleaseInfoText={true}
-                    handleClick={() => {
-                        props.setSelectedIndex(i)
-                        setSearchDisplayed(true)
-                    }}
-                />
-            </Col>
-        )
-    }
-    return columns
+  for (let i = 0; i < 6; i++) {
+    // Generates JSX tags for album art
+    columns.push(
+      <Col key={i}>
+        <CoverArt
+          albumData={props.profile.favoriteAlbums[i]}
+          isClickable={isClickable}
+          fontSize={11}
+          displayReleaseInfoText={true}
+          handleClick={() => {
+            props.setSelectedIndex(i);
+            setSearchDisplayed(true);
+          }}
+        />
+      </Col>
+    );
+  }
+  return columns;
 }
 
 function FavoriteAlbums(props) {
+  useEffect(() => {
+    return () => {
+      setSearchDisplayed(false);
+    };
+  }, [props.id]);
 
-    useEffect(() => {
-        return () => {
-            setSearchDisplayed(false)
-        }
-    }, [props.id])
+  const { isProfileOwner } = props;
+  const [searchDisplayed, setSearchDisplayed] = useState(false);
 
-    const {isProfileOwner} = props
-    const [searchDisplayed, setSearchDisplayed] = useState(false)
-
-    return (
-        <>
-            <Container>
-                <Row xs={6}>
-                    {generateAlbumTags(props, setSearchDisplayed, isProfileOwner)}
-                </Row>
-            </Container>
-            <div>
-                {searchDisplayed && <Search
-                    clickFunction={props.setFavAlbum}
-                    clearSearchVisibility={() => {
-                        setSearchDisplayed(false)
-                    }}
-                    clickFunctionArgs={[props.selectedIndex]}
-                />}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Container>
+        <Row xs={6}>
+          {generateAlbumTags(props, setSearchDisplayed, isProfileOwner)}
+        </Row>
+      </Container>
+      <div>
+        {searchDisplayed && (
+          <Search
+            clickFunction={props.setFavAlbum}
+            clearSearchVisibility={() => {
+              setSearchDisplayed(false);
+            }}
+            clickFunctionArgs={[props.selectedIndex]}
+          />
+        )}
+      </div>
+    </>
+  );
 }
 
 FavoriteAlbums.propTypes = {
-    isProfileOwner: PropTypes.bool.isRequired,
-}
+  isProfileOwner: PropTypes.bool.isRequired,
+};
 
-const mapStateToProps = state => ({
-    id: state.spotify.id,
-    profile: state.profile,
-    selectedIndex: state.spotifySearch.selectedIndex,
-})
+const mapStateToProps = (state) => ({
+  id: state.spotify.id,
+  profile: state.profile,
+  selectedIndex: state.spotifySearch.selectedIndex,
+});
 
 export default connect(mapStateToProps, {
-    setFavAlbum,
-    setSelectedIndex,
-})(FavoriteAlbums)
+  setFavAlbum,
+  setSelectedIndex,
+})(FavoriteAlbums);
